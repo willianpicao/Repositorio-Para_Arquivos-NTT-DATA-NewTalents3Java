83258ff585808e346466834724fbded900e5888a
Willian <williapicao6@gmail.com>
2021-10-28 13:11:26 -0300

    first commit

---
+++ src/br/com/dio/primeiroPrograma.java
@@ -0,0 +1,10 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class primeiroPrograma {
+    public static void main(String[] args) {
+//                  System.out.println("Hellow World");
+        Gato gato = new Gato();
+    }
+}
