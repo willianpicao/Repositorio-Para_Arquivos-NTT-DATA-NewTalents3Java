56c86cca090ee6cda41fcf40449b2e9774435277
Willian <williapicao6@gmail.com>
2021-11-01 20:01:45 -0300

    commit de atualização

---
+++ EstruturaDeDadosEmJava/src/com/projeto/projetoNo/src/main/java/com/projetoNo/Main.java
@@ -0,0 +1,28 @@
+package com.projetoNo;
+
+public class Main {
+    public static void main(String[] args){
+        No no1 = new No("conteúdo no1");
+        No no2 = new No("conteúdo no2");
+
+        no1.setProximoNo(no2);
+
+        No no3 = new No("conteudo no3");
+        no2.setProximoNo(no3);
+
+        No no4 = new No("conteudo no4");
+        no3.setProximoNo(no4);
+
+        //no1->no2->no3->no4->null
+         System.out.println(no1);
+         System.out.println(no1.getProximoNo());
+
+        System.out.println("...................................");
+
+        System.out.println(no1);
+        System.out.println(no1.getProximoNo());
+        System.out.println(no1.getProximoNo().getProximoNo());
+        System.out.println(no1.getProximoNo().getProximoNo().getProximoNo());
+        System.out.println(no1.getProximoNo().getProximoNo().getProximoNo().getProximoNo());
+    }
+}
